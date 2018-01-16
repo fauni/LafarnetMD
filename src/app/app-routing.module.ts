d1ba014f99d4ac92035f376ec78c5c478da7e8ce
FRANZ-PC\Franz Aruni <amrfranz@gmail.com>
2018-01-16 08:36:43 -0400

    Modulo Usuarios Login Dinamico OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AuthGuard } from './shared';
 
 const routes: Routes = [
     { path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] },
+    { path: 'admin', loadChildren: './admin-intranet/admin-intranet.module#AdminIntranetModule', canActivate: [AuthGuard] },
     { path: 'login', loadChildren: './login/login.module#LoginModule' },
     { path: 'error', loadChildren: 'app/server-error/server-error.module#ServerErrorModule' },
     { path: 'access-denied', loadChildren: 'app/access-denied/access-denied.module#AccessDeniedModule' },
