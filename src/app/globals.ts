c5b6b51e44c8edf72b6e668d64a356327f54eff0
severo cruz <severo_86@hotmail.com>
2018-03-15 10:30:21 -0400

    cambio de globals severo

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -12,8 +12,6 @@ export class Globals {
     urlImagenesPersonal: string = 'http://intranet.lafar.net:8080/newApiLafarnet/assets/imagenes_users/';
     urlPublicaciones: string = 'http://intranet.lafar.net:8080/newApiLafarnet/assets/publicaciones_images/';*/
 
-
-    
     urlAPI: string = 'http://localhost:8080/newapilafarnet/public/';
     urlImagenesPersonal: string = 'http://localhost:8080/newApiLafarnet/assets/imagenes_users/';
     urlPublicaciones: string = 'http://localhost:8080/newapilafarnet/assets/publicaciones_images/';
