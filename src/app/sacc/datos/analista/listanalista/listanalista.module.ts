85ef3f3e666622d72dcfb8f2d76ff85b026b95b2
severo cruz <severo_86@hotmail.com>
2018-03-22 19:24:58 -0400

    AGREGACION MODAL NUEVO ANALISTA

--- src/app/sacc/datos/analista/listanalista/listanalista.module.ts
+++ src/app/sacc/datos/analista/listanalista/listanalista.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListanalistaRoutingModule } from './listanalista-routing.module';
 import { ListanalistaComponent } from './listanalista.component';
-
+import { MzModalModule, MzButtonModule, MzInputModule, MzCardModule } from 'ng2-materialize';
+import { FormsModule } from '@angular/forms';
 @NgModule({
     imports: [
         CommonModule,
-        ListanalistaRoutingModule
+        ListanalistaRoutingModule,
+        MzModalModule,
+        MzButtonModule,
+        FormsModule,
+        MzInputModule,
+        MzCardModule
     ],
     declarations: [ListanalistaComponent]
 })
 
-export class ListanalistaModule {}
+export class ListanalistaModule {
+}
