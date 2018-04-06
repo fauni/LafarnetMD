645df22386c840b2d7dbb938e9876179cacd4db5
FRANZ-PC\Franz Aruni <amrfranz@gmail.com>
2018-04-06 17:37:03 -0400

    CARACTERISTICAS OBJETO

---
+++ src/app/sacc/asignacion/caracteristicas.ts
@@ -0,0 +1,8 @@
+export class Caracteristicas {
+    public codigo_producto: string;
+    public descripcion: string;
+    public especificacion: string;
+    public id_caracteristica: string;
+    public id_productos_especificacion: string;
+    public tipo_caracteristica: string;
+}
