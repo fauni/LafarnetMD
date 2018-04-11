504151b17fb78364ca35d0838cc0c3a382019350
severo cruz <severo_86@hotmail.com>
2018-04-11 15:33:03 -0400

    parametros asignacion con filtro  2

--- src/app/sacc/asignacion/asignacionparm/asignacionparm.component.ts
+++ src/app/sacc/asignacion/asignacionparm/asignacionparm.component.ts
@@ -17,6 +17,7 @@ export class AsignacionparmComponent implements OnInit {
 
   constructor(public global: Globals, public servClasificaciones: ClasificacionService,
              public servClasificacionCaracteristica: ClasificacionCaracteristicaService) { }
+    public swich = 0 ; // verificacion en la llamada a la funcion onclick
     public tipo_producto = '1'; // para el radio button, puede ser 1 = MP:materia prima o 2 = PT: producto terminado
     public clasificacion: Clasificacion;
     public clasificaciones: Array<Clasificacion>;
@@ -24,6 +25,12 @@ export class AsignacionparmComponent implements OnInit {
     public clasificacionescaracteristicas: Array<ClasificacionCaracteristica>;
     public clasificacioncaracteristicaCFAQCM: ClasificacionCaracteristicaCFAQCM;
     public clasificacionescaracteristicasCFAQCM: Array<ClasificacionCaracteristicaCFAQCM>;
+    public clasificacioncaracteristicaCF: ClasificacionCaracteristicaCFAQCM;
+    public clasificacionescaracteristicasCF: Array<ClasificacionCaracteristicaCFAQCM>;
+    public clasificacioncaracteristicaAQ: ClasificacionCaracteristicaCFAQCM;
+    public clasificacionescaracteristicasAQ: Array<ClasificacionCaracteristicaCFAQCM>;
+    public clasificacioncaracteristicaCM: ClasificacionCaracteristicaCFAQCM;
+    public clasificacionescaracteristicasCM: Array<ClasificacionCaracteristicaCFAQCM>;
     public tipoClasificacionProducto = 'A'; // A o B o C.....F
   ngOnInit() {
     this.onLoadClasificaciones();
@@ -74,6 +81,7 @@ export class AsignacionparmComponent implements OnInit {
           this.clasificacionescaracteristicasCFAQCM = data.body;
           console.log('clasificaciones caracteristicas con CF AQ CM');
           console.log(this.clasificacionescaracteristicasCFAQCM);
+          this.onLoadClasificacionesCaracteristicasCFAQCMporseparado();
         },
         (err: HttpErrorResponse) => {
           if (err.error instanceof Error) {
@@ -87,9 +95,34 @@ export class AsignacionparmComponent implements OnInit {
         }
         );
       }
+      onLoadClasificacionesCaracteristicasCFAQCMporseparado() {
+        this.clasificacionescaracteristicasCF = new Array<ClasificacionCaracteristicaCFAQCM>();
+        this.clasificacionescaracteristicasAQ = new Array<ClasificacionCaracteristicaCFAQCM>();
+        this.clasificacionescaracteristicasCM = new Array<ClasificacionCaracteristicaCFAQCM>();
+        this.clasificacionescaracteristicasCFAQCM.forEach(element => {
+           if (element.tipo_caracteristica == 'CF') {
+            this.clasificacionescaracteristicasCF.push(element);
+           }
+           if (element.tipo_caracteristica == 'AQ') {
+            this.clasificacionescaracteristicasAQ.push(element);
+           }
+           if (element.tipo_caracteristica == 'CM') {
+            this.clasificacionescaracteristicasCM.push(element);
+           }
+        });
+      }
+
    onClicked(tipo) {
-    this.filtrarportipo(tipo);
     this.asignatipo(tipo);
+    this.filtrarportipo(tipo);
+    if (this.swich > 0) {
+     this.onLoadClasificacionesCaracteristicasCFAQCMporseparado();
+    }
+    this.swich++;
+    console.log('************************');
+    console.log(this.clasificacionescaracteristicasCF);
+    console.log(this.clasificacionescaracteristicasAQ);
+    console.log(this.clasificacionescaracteristicasCM);
    }
    filtrarportipo(tipo) {
      let tipolit: string;
