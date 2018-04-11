1353d456886fad1dbfd363a8190ea94f52f34ea5
FRANZ-PC\Franz Aruni <amrfranz@gmail.com>
2018-04-11 09:56:44 -0400

    ADICIONAR CABECERA CERTIFICADOS

--- src/app/sacc/certificados/certificados.service.ts
+++ src/app/sacc/certificados/certificados.service.ts
@@ -11,6 +11,10 @@ export class CertificadosService {
   private headers2 = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
   constructor(private http: HttpClient, private global: Globals) { }
 
+  setCertificados(data): Observable<any> {
+    return this.http.post(this.global.urlAPI + 'sacccertificadoanalisis/save', data);
+  }
+
   getCertificados() {
     return this.http.get(this.global.urlAPI + 'sacccertificadoanalisis/pt');
   }
