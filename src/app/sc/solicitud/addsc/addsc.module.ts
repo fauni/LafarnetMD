21f4258725b2b526f4a8b9c715777235f95e113d
faruni <amrfranz@gmail.com>
2018-12-10 08:34:04 -0400

    SAVE UPLOAD FILE OK

--- src/app/sc/solicitud/addsc/addsc.module.ts
+++ src/app/sc/solicitud/addsc/addsc.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AddscRoutingModule } from './addsc-routing.module';
 import { AddscComponent } from './addsc.component';
 import { MzCardModule, MzButtonModule, MzIconModule, MzNavbarModule, MzInputModule, MzSelectModule, MzDatepickerModule,
-    MzIconMdiModule, MzTextareaModule, MzModalModule, MzToastService, MzToastModule, MzValidationModule, MzCollectionModule } from 'ng2-materialize';
+    MzIconMdiModule, MzTextareaModule, MzModalModule, MzToastService, MzToastModule, MzValidationModule, MzCollectionModule, MzBadgeModule } from 'ng2-materialize';
 import { SolicitudService } from '../solicitud.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
@@ -27,7 +27,8 @@ import { NgSelectModule } from '@ng-select/ng-select';
         ReactiveFormsModule,
         MzValidationModule,
         NgSelectModule,
-        MzCollectionModule
+        MzCollectionModule,
+        MzBadgeModule
     ],
     declarations: [
         AddscComponent
